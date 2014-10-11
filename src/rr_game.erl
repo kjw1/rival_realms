0a2b98da7bc7991dc851f11bf842fef3c045ce68
Kelly Wong <kjw0188@gmail.com>
2014-10-11 01:54:01 -0700

    Start making card plays more general

--- src/rr_game.erl
+++ src/rr_game.erl
@@ -1,5 +1,5 @@
 -module(rr_game).
--export([init_rand/0, init_card_defs/0, new_game/0, get_card/1, shuffle_list/1, play_card/6]).
+-export([init_rand/0, init_card_defs/0, new_game/0, get_card_def/1, shuffle_list/1, make_play/4]).
 -export([check_attackers/3, check_defenders/4]).
 
 -ifdef(TEST).
@@ -9,9 +9,11 @@
 -type card_type() :: supply | unit | realm.
 -record(game, {players = []}).
 -record(player, {id, hand = [], discard = [], deck = [], lost = [], field = [], life = 20, supply = 0}).
--record(card_def, {key, cost = 0, reqs=[], type = supply :: card_type(), attrs = []}).
--type card_attachment() :: any()
--record(card, {key, type, attached}).
+-record(cost, {supply= 0, discards = [], burns=[]}).
+-record(card_def, {key, cost :: #cost{}, reqs=[], type = supply :: card_type(), attrs = []}).
+-type card_attachment() :: tuple().
+-record(card, {id, type, attached :: [card_attachment()], attacking=false}).
+-record(ability, {phases, cost :: #cost{}, effect}).
 
 init_rand() ->
   <<A:32,B:32,C:32>> = crypto:rand_bytes(12),
@@ -38,7 +40,7 @@ init_card_defs() ->
   ets:insert(card_defs, supply_card()),
   ets:insert(card_defs, soldier_card()).
 
-get_card(Id) ->
+get_card_def(Id) ->
   [#card_def{} = Card] =  ets:lookup(card_defs, Id),
   Card.
 
@@ -51,49 +53,108 @@ pile_for_player(#player{deck=Deck}, deck) -> Deck;
 pile_for_player(#player{lost=Lost}, lost) -> Lost;
 pile_for_player(#player{field=Field}, field) -> Field.
 
+ability_from_card(#card_def{attrs=Attrs}, AbilityId) ->
+  {abilities, Abilities} = proplists:lookup(abillities, Attrs),
+  lists:nth(AbilityId, Abilities).
+
+get_card_or_action(play_hand, PlayerId, CardId, #game{} = Game) ->
+  card_from_game({PlayerId, hand, CardId}, Game);
+get_card_or_action(activate, _PlayerId, {CardLocation, AbilityId}, #game{} = Game) ->
+  #card{type=CardDef} = card_from_game(CardLocation, Game),
+  ability_from_card(CardDef, AbilityId).
+
 card_from_game({Player, Where, Id}, #game{players=Players}) ->
   PlayerState = lists:keyfind(Player, #player.id, Players),
   Pile = pile_for_player(PlayerState, Where),
-  {Id, Card} = lists:keyfind(Id, 1, Pile),
-  get_card(Card).
+  lists:keyfind(Id, #card.id, Pile).
 
-%Can always play supply cards
-check_allowed(_Phase, Player, _CurPlayer,
-              {Player, hand, _Id}, #card_def{type=supply}, {Player, discard}, _Game) ->
+check_controls_card(play_hand, _CardId, _Player) ->
   allowed;
-%Can deploy units on own deploy phase
-%If have enough supply for card
-check_allowed(deploy, Player, Player,
-              {Player, hand, _Id}, #card_def{cost=Cost, type=unit}, {Player, field}, Game) ->
-  #player{supply=Supply} = get_player(Game, Player),
-  case Supply of
-    Supply when Supply >= Cost -> allowed;
-    _Supply -> not_allowed
-  end;
-%Anything else not allowed
-check_allowed(_Phase, _Player, _CurPlayer, _From, _Card, _To, _Game) ->
+check_controls_card(activate, {{PlayerId, _Pile, _CardId}, _AbilityId}, PlayerId) ->
+  allowed;
+check_controls_card(_Action, _Source, _PlayerId) ->
   not_allowed.
 
-play_card(Phase, Player, From, To, CurPlayer, Game) ->
-  Card = card_from_game(From, Game),
-  Allowed = check_allowed(Phase, Player, CurPlayer, From, Card, To, Game),
-  UpdatedGame = play_card_if_allowed(Allowed, Card, From, To, Game),
+check_phase(CurPhase, PlayerId, PlayerId, #ability{phases=Phases}) ->
+  case lists:any(fun(Phase) -> Phase =:= CurPhase end, Phases) of
+    true -> allowed;
+    false -> not_allowed
+  end;
+check_phase({reinforce, PlayerId}, PlayerId, _CurPlayer, #ability{phases=Phases}) ->
+  case lists:any(fun(Phase) -> Phase =:= reinforce end, Phases) of
+    true -> allowed;
+    false -> not_allowed
+  end;
+check_phase({reinforce, PlayerId}, PlayerId, _CurPlayer, #card_def{type=reinforce}) ->
+  allowed;
+check_phase(deploy, PlayerId, PlayerId, #card_def{type=unit}) ->
+  allowed;
+check_phase(deploy, PlayerId, PlayerId, #card_def{type=realm}) ->
+  allowed.
+
+get_card_or_ability_cost(#ability{cost=Cost}) ->
+  Cost;
+get_card_or_ability_cost(#card{type=Type}) ->
+  #card_def{cost=Cost} = get_card_def(Type),
+  Cost.
+
+check_cost(PlayerId, CostTargets, CardOrAbility, #game{}=Game) ->
+  Player = get_player(Game, PlayerId),
+  #cost{supply=Supply, discards=Discards, burns=Burns} = get_card_or_ability_cost(CardOrAbility),
+  HasSupply = check_player_has_supply(Player, Supply),
+  CostTargetsOk = check_cost_targets(CostTargets, Discards, Burns),
+  HasSupply and CostTargetsOk.
+
+check_player_has_supply(#player{supply=Supply}, SupplyCost) ->
+  Supply >= SupplyCost.
+
+%% TODO Fix this later
+check_cost_targets({Discards, Burns}, ReqDiscards, ReqBurns) ->
+  true.
+
+check_targets(CardOrAction, AbilityTargets, Game) ->
+  allowed.
+
+%Can always play supply cards
+check_allowed(Phase, {Action, Player, Source, CostTargets, AbilityTargets}, CurPlayer, Game) ->
+  CardOrAction = get_card_or_action(Action, Player, Source, Game),
+  SourceOk = check_controls_card(Action, Source, Player),
+  PhaseOk = check_phase(Phase, Player, CurPlayer, CardOrAction),
+  CostOk = check_cost(Player, CostTargets, CardOrAction, Game),
+  TargetsOk = check_targets(CardOrAction, AbilityTargets, Game),
+  check_all_pass(SourceOk, PhaseOk, CostOk, TargetsOk).
+
+check_all_pass(allowed, allowed, allowed, allowed) -> allowed;
+check_all_pass(_Fail0, _Fail1, _Fail2, _Fail3) -> not_allowed.
+
+-type player_id() :: integer().
+-type card_id() :: integer().
+-type ability_index() :: integer().
+-type pile() :: hand | field | discard | deck | lost.
+-type card_location() :: {player_id(), pile(), card_id()}.
+-type targets() :: [ card_location() ] | none | {player_id(), pile()}.
+-type game_action() :: { activate, player_id(), {card_location(), ability_index()}, {targets(), targets()}, targets()} | 
+  { play_hand, player_id(), card_id(), { targets(),  targets()}, targets()}.
+
+-spec make_play(atom(), game_action(), player_id(), #game{}) -> {allowed | not_allowed, #game{} }.
+make_play(Phase, Action, CurPlayer, Game) ->
+  Allowed = check_allowed(Phase, Action, CurPlayer, Game),
+  UpdatedGame = play_card_if_allowed(Allowed, Action, Game),
   {Allowed, UpdatedGame}.
 
-play_card_if_allowed(not_allowed, _Card, _From, _To, Game) ->
+play_card_if_allowed(not_allowed, Action, Game) ->
   Game;
-play_card_if_allowed(allowed, #card_def{type=unit, cost=Cost}, 
-                     From, {Player, field}=To, #game{}=Game) ->
-  {Card, RemovedGame} = remove_card(Game, From),
-  PaidSupply = add_supply(Player, - Cost, RemovedGame),
-  add_card(PaidSupply, Card, To);
-play_card_if_allowed(allowed, #card_def{type=supply, attrs=Attrs}, 
-                     From, {Player, discard}=To, #game{}=Game) ->
-  {supply, Supply} = lists:keyfind(supply, 1, Attrs),
-  {Card, RemovedGame} = remove_card(Game, From),
-  AddedGame = add_card(RemovedGame, Card, To),
-  UpdatedSupply = add_supply(Player, Supply, AddedGame),
-  UpdatedSupply.
+play_card_if_allowed(allowed, {Action, Player, Source, CostTargets, AbilityTargets}, #game{}=Game) ->
+  CardOrAction = get_card_or_action(Action, Player, Source, Game),
+  PaidCost = pay_cost(Player, get_card_or_ability_cost(CardOrAction), CostTargets, Game),
+  do_play_effect(Action, Player, CardOrAction, Source, AbilityTargets, PaidCost).
+
+pay_cost(Player, Cost, CostTargets, Game) ->
+  Game.
+
+%TODO Actually do the effect of the play
+do_play_effect(Action, Player, CardOrAction, Source, AbilityTargets, Game) ->
+  Game.
 
 add_supply(Player, Supply, Game) ->
   PlayerState = get_player(Game, Player),
@@ -156,7 +217,7 @@ check_attackers_in_field(Field, Attackers) ->
     lists:all(fun(Attacker) ->
             {Attacker, CardType} = lists:keyfind(Attacker, 1, Field),
             %io:format("Got card: ~p~n", [get_card(CardType)]),
-            Card = get_card(CardType),
+            Card = get_card_def(CardType),
             unit =:= Card#card_def.type
         end, Attackers)
   catch
